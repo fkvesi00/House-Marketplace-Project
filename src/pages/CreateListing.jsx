dd2cba3d0232334f505661ed26b9042cd2fd78c0
fkvesi00 <fkvesi00@fesb.hr>
2024-06-11 18:55:28 +0200

    adding geolocation from google

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react"
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import { useNavigate } from "react-router-dom"
 import Spinner from "../components/Spinner"
+import { toast } from "react-toastify"
 
 
 function CreateListing() {
@@ -43,9 +44,39 @@ function CreateListing() {
 
   },[isMounted])
 
-  const onSubmit = e => {
+  const onSubmit =async e => {
     e.preventDefault()
-    console.log(formData)
+    setLoading(true)
+    if(discountedPrice >= regularPrice){
+      setLoading(false)
+      toast.error('Make discounted price lower than regular price')
+      return
+    }
+
+    if(images.length > 6 ){
+      setLoading(false)
+      toast.error('Max 6 img')
+      return
+    }
+
+    let geolocation = {}
+    let location 
+
+    if(geoLocationEnabled){
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAkMXmyyMmVA76iUszZlEv8VaDt54vu2ec`)
+
+      const data = await response.json()
+      geolocation.lat = data.results[0].geometry.location.lat ?? 0
+
+      geolocation.lng = data.results[0].geometry.location.lng ?? 0
+    }else{
+      geolocation.lat = latitude
+      geolocation.lng = longitude
+      location = address
+    }
+
+    setLoading(false)
+
   }
 
   const onMutate = e => {
